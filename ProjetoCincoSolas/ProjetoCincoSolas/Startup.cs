10a83083d459eeba548d11f93c05ab4facac43fc
wellington.sales <wellington.sales@INT73>
2019-01-08 12:44:45 -0200

    Criação do Projeto

---
+++ ProjetoCincoSolas/ProjetoCincoSolas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoCincoSolas.Startup))]
+namespace ProjetoCincoSolas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
