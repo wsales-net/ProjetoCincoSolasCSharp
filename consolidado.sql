01be33c1f23a80da32609019ffbd29375c8eba48
wsales-net <31112665+wsales-net@users.noreply.github.com>
2019-04-10 12:34:01 -0300

    criação da tabela de email para pessoa

--- consolidado.sql
+++ consolidado.sql
@@ -1,3 +1,5 @@
+-- VERIFICAR ID TELEFONE E TABELA TELEFONE INTEGRAÇÃO
+
 GO
  -- EXEMPLO: LIVRO, PREGAÇÃO, WHATSAPP-AUDIO, PODCAST, SITE
 If Object_Id('CategoriaAnotacaoEvangelho') Is Null
@@ -72,26 +74,53 @@ CREATE TABLE AnotacaoEvangelhoTag
 )
 
 GO
+
 If Object_Id('Pessoa') Is Null
 CREATE TABLE Pessoa (
 	Id INT IDENTITY,
-	IdEnderecoPessoa INT NOT NULL,
-	IdFuncaoMinisterial INT NOT NULL,
 	IdTelefone INT NOT NULL,
 	IdEstadoCivil INT NOT NULL,
+	IdEnderecoPessoa INT NOT NULL,
+	IdFuncaoMinisterial INT NOT NULL,
 	Nome VARCHAR(15) NOT NULL,
 	SobreNome VARCHAR(60) NOT NULL,
 	Sexo CHAR(1) NOT NULL,
-	DataNascimento datetime NOT NULL,
+	DataNascimento DATETIME NOT NULL,
 	NomeMae VARCHAR(30),
 	NomePai VARCHAR(30),
 	RG VARCHAR(12) UNIQUE,
 	CPF VARCHAR(14) UNIQUE,
-	Email VARCHAR(30) UNIQUE,
-	DataRegistro datetime NOT NULL,
+	DataRegistro DATETIME NOT NULL,
 	Ativo BIT NOT NULL,
 	Foto VARCHAR,
   	CONSTRAINT PkPessoa PRIMARY KEY (Id)
+)
+
+GO
+
+If Object_Id('IdTipoEmail') Is Null
+CREATE TABLE IdTipoEmail (
+	Id INT NOT NULL,
+	TipoEmail VARCHAR(15) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
+  	CONSTRAINT PkTipoEmail PRIMARY KEY (Id)
+)
+
+GO
+
+If Object_Id('Email') Is Null
+CREATE TABLE Email (
+	Id INT IDENTITY,
+	IdPessoa INT NOT NULL,
+	IdTipoEmail INT NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	Observacao VARCHAR(500),
+	EmailPrincipal BIT NOT NULL,
+	Excluido BIT NOT NULL DEFAULT (0),
+	Notificado BIT NOT NULL DEFAULT (0),
+	Confirmado BIT NOT NULL DEFAULT (0),
+	DataRegistro DATETIME DEFAULT GETDATE(),
+  	CONSTRAINT PkFuncaoMinisterial PRIMARY KEY (Id)
 )
 
 GO
@@ -100,6 +129,7 @@ If Object_Id('FuncaoMinisterial') Is Null
 CREATE TABLE FuncaoMinisterial (
 	Id INT NOT NULL,
 	Descricao VARCHAR(15) NOT NULL UNIQUE,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkFuncaoMinisterial PRIMARY KEY (Id)
 )
 
@@ -110,8 +140,9 @@ CREATE TABLE Dizimo (
 	Id INT NOT NULL,
 	IdPessoa INT NOT NULL,
 	Valor decimal(6,2) NOT NULL,
-	DataEntrada datetime NOT NULL,
+	DataEntrada DATETIME NOT NULL,
 	Observacao VARCHAR(500),
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkDizimo PRIMARY KEY (Id)
 )
 
@@ -124,6 +155,7 @@ CREATE TABLE Usuario (
 	IdPessoa INT NOT NULL,
 	Usuario VARCHAR(20) NOT NULL UNIQUE,
 	Senha VARCHAR(30) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkUsuario PRIMARY KEY (Id)
 )
 
@@ -136,6 +168,7 @@ CREATE TABLE Venda (
 	Valor decimal(5,2) NOT NULL,
 	Desconto decimal(5,2) NOT NULL,
 	ValorPago decimal(5,2) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkVenda PRIMARY KEY (Id)
 )
 
@@ -146,6 +179,7 @@ CREATE TABLE Cidade (
 	Id INT NOT NULL,
 	IdEstado INT NOT NULL,
 	Descricao VARCHAR(100) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkCidade PRIMARY KEY (Id)
 )
 
@@ -155,6 +189,7 @@ If Object_Id('Estado') Is Null
 CREATE TABLE Estado (
 	Id INT NOT NULL,
 	UF VARCHAR(2) NOT NULL UNIQUE,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkEstado PRIMARY KEY (Id)
 )
 
@@ -169,6 +204,7 @@ CREATE TABLE Endereco (
 	Bairro VARCHAR(70) NOT NULL,
 	Numero INT NOT NULL,
 	Complemento VARCHAR(20),
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkEndereco PRIMARY KEY (Id)
 )
 
@@ -179,8 +215,9 @@ CREATE TABLE Oferta (
 	Id INT NOT NULL,
 	IdPessoa INT,
 	Valor decimal(6,2) NOT NULL,
-	DataEntrada datetime NOT NULL,
+	DataEntrada DATETIME NOT NULL,
 	Observacao VARCHAR(500),
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkOferta PRIMARY KEY (Id)
 )
 
@@ -193,6 +230,7 @@ CREATE TABLE ItemVenda (
 	IdProduto INT NOT NULL,
 	Quantidade INT NOT NULL,
 	Valor decimal(5,2) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkItemVenda PRIMARY KEY (Id)
 )
 
@@ -202,6 +240,7 @@ If Object_Id('Categoria') Is Null
 CREATE TABLE Categoria (
 	Id INT NOT NULL,
 	Descricao VARCHAR(20) NOT NULL UNIQUE,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkCategoria PRIMARY KEY (Id)
 )
 
@@ -214,6 +253,7 @@ CREATE TABLE Produto (
 	Nome VARCHAR(30) NOT NULL UNIQUE,
 	Valor decimal(5,2) NOT NULL,
 	Descricao VARCHAR(500) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkProduto PRIMARY KEY (Id)
 )
 
@@ -225,8 +265,9 @@ CREATE TABLE ContaReceber (
 	IdVenda INT NOT NULL,
 	IdFormaPagamento INT NOT NULL,
 	Valor decimal(6,2) NOT NULL,
-	DataEntrada datetime NOT NULL,
+	DataEntrada DATETIME NOT NULL,
 	Observacao VARCHAR(500),
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkContaReceber PRIMARY KEY (Id)
 )
 
@@ -236,6 +277,7 @@ If Object_Id('FormaPagamento') Is Null
 CREATE TABLE FormaPagamento (
 	Id INT NOT NULL,
 	Descricao VARCHAR(15) NOT NULL UNIQUE,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkFormaPagamento PRIMARY KEY (Id)
 )
 
@@ -246,6 +288,7 @@ CREATE TABLE Publico (
 	Id INT NOT NULL,
 	Descricao VARCHAR(10) NOT NULL UNIQUE,
 	Classificacao VARCHAR(5) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkPublico PRIMARY KEY (Id)
 )
 
@@ -262,6 +305,7 @@ CREATE TABLE Evento (
 	Descricao VARCHAR(500),
 	Numero INT NOT NULL,
 	PontoReferencia VARCHAR(30),
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkEvento PRIMARY KEY (Id)
 )
 
@@ -273,6 +317,7 @@ CREATE TABLE Telefone (
 	IdEstado INT NOT NULL,
 	Numero VARCHAR(10) NOT NULL,
 	Servico VARCHAR(10) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkTelefone PRIMARY KEY (Id)
 )
 
@@ -283,6 +328,7 @@ CREATE TABLE Orcamento (
 	Id INT NOT NULL,
 	TotalDizimo decimal(6,2) NOT NULL,
 	TotalOferta decimal(6,2) NOT NULL,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkOrcamento PRIMARY KEY (Id)
 )
 
@@ -294,8 +340,9 @@ CREATE TABLE ContaPagar (
 	IdTipoConta INT NOT NULL,
 	Descricao VARCHAR(10) NOT NULL,
 	Valor decimal(6,2) NOT NULL,
-	DataVencimento datetime NOT NULL,
-	DataPagamento datetime,
+	DataVencimento DATETIME NOT NULL,
+	DataPagamento DATETIME,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkContaPagar PRIMARY KEY (Id)
 )
 
@@ -305,6 +352,7 @@ If Object_Id('TipoContaPagar') Is Null
 CREATE TABLE TipoContaPagar (
 	Id INT NOT NULL,
 	Descricao VARCHAR(10) NOT NULL UNIQUE,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkTipoContaPagar PRIMARY KEY (Id)
 )
 
@@ -314,94 +362,104 @@ If Object_Id('Perfil') Is Null
 CREATE TABLE Perfil (
 	Id INT NOT NULL,
 	Nome VARCHAR(20) NOT NULL UNIQUE,
+	DataRegistro DATETIME DEFAULT GETDATE(),
   	CONSTRAINT PkPerfil PRIMARY KEY (Id)
 )
 
 GO
-ALTER TABLE [Pessoa] WITH CHECK ADD CONSTRAINT [FkPessoa_x_Endereco] FOREIGN KEY ([IdEnderecoPessoa]) REFERENCES [Endereco]([Id])
+ALTER TABLE Pessoa WITH CHECK ADD CONSTRAINT FkPessoa_x_Endereco FOREIGN KEY (IdEnderecoPessoa) REFERENCES Endereco(Id)
 GO
-ALTER TABLE [Pessoa] CHECK CONSTRAINT [FkPessoa_x_Endereco]
+ALTER TABLE Pessoa ADD CONSTRAINT CHK_PessoaSexo CHECK (SEXO = 'M' OR SEXO = 'F');
 GO
-ALTER TABLE [Pessoa] WITH CHECK ADD CONSTRAINT [FkPessoa_x_FuncaoMinisterial] FOREIGN KEY ([IdFuncaoMinisterial]) REFERENCES [FuncaoMinisterial]([Id])
+ALTER TABLE Pessoa CHECK CONSTRAINT FkPessoa_x_Endereco
 GO
-ALTER TABLE [Pessoa] CHECK CONSTRAINT [FkPessoa_x_FuncaoMinisterial]
+ALTER TABLE Pessoa WITH CHECK ADD CONSTRAINT FkPessoa_x_FuncaoMinisterial FOREIGN KEY (IdFuncaoMinisterial) REFERENCES FuncaoMinisterial(Id)
 GO
-ALTER TABLE [Pessoa] WITH CHECK ADD CONSTRAINT [FkPessoa_x_Telefone] FOREIGN KEY ([IdTelefone]) REFERENCES [Telefone]([Id])
+ALTER TABLE Pessoa CHECK CONSTRAINT FkPessoa_x_FuncaoMinisterial
 GO
-ALTER TABLE [Pessoa] CHECK CONSTRAINT [FkPessoa_x_Telefone]
+ALTER TABLE Pessoa WITH CHECK ADD CONSTRAINT FkPessoa_x_Telefone FOREIGN KEY (IdTelefone) REFERENCES Telefone(Id)
+GO
+ALTER TABLE Pessoa CHECK CONSTRAINT FkPessoa_x_Telefone
 
 GO
-ALTER TABLE [Dizimo] WITH CHECK ADD CONSTRAINT [FkDizimo_x_Pessoa] FOREIGN KEY ([IdPessoa]) REFERENCES [Pessoa]([Id])
+ALTER TABLE Email WITH CHECK ADD CONSTRAINT FkEmail_x_Pessoa FOREIGN KEY (IdPessoa) REFERENCES Telefone(Id)
 GO
-ALTER TABLE [Dizimo] CHECK CONSTRAINT [FkDizimo_x_Pessoa]
+ALTER TABLE Email CHECK CONSTRAINT FkEmail_x_Pessoa
+GO
+CREATE UNIQUE INDEX UQ_Email ON dbo.Email(IdPessoa, Email);
 
 GO
-ALTER TABLE [Usuario] WITH CHECK ADD CONSTRAINT [FkUsuario_x_Perfil] FOREIGN KEY ([IdPerfil]) REFERENCES [Perfil]([Id])
+ALTER TABLE Dizimo WITH CHECK ADD CONSTRAINT FkDizimo_x_Pessoa FOREIGN KEY (IdPessoa) REFERENCES Pessoa(Id)
 GO
-ALTER TABLE [Usuario] CHECK CONSTRAINT [FkUsuario_x_Perfil]
-GO
-ALTER TABLE [Usuario] WITH CHECK ADD CONSTRAINT [FkUsuario_x_Pessoa] FOREIGN KEY ([IdPessoa]) REFERENCES [Pessoa]([Id])
-GO
-ALTER TABLE [Usuario] CHECK CONSTRAINT [FkUsuario_x_Pessoa]
+ALTER TABLE Dizimo CHECK CONSTRAINT FkDizimo_x_Pessoa
 
 GO
-ALTER TABLE [Venda] WITH CHECK ADD CONSTRAINT [FkVenda_x_Pessoa] FOREIGN KEY ([IdPessoa]) REFERENCES [Pessoa]([Id])
+ALTER TABLE Usuario WITH CHECK ADD CONSTRAINT FkUsuario_x_Perfil FOREIGN KEY (IdPerfil) REFERENCES Perfil(Id)
 GO
-ALTER TABLE [Venda] CHECK CONSTRAINT [FkVenda_x_Pessoa]
+ALTER TABLE Usuario CHECK CONSTRAINT FkUsuario_x_Perfil
+GO
+ALTER TABLE Usuario WITH CHECK ADD CONSTRAINT FkUsuario_x_Pessoa FOREIGN KEY (IdPessoa) REFERENCES Pessoa(Id)
+GO
+ALTER TABLE Usuario CHECK CONSTRAINT FkUsuario_x_Pessoa
 
 GO
-ALTER TABLE [Cidade] WITH CHECK ADD CONSTRAINT [FkCidade_x_Estado] FOREIGN KEY ([IdEstado]) REFERENCES [Estado]([Id])
+ALTER TABLE Venda WITH CHECK ADD CONSTRAINT FkVenda_x_Pessoa FOREIGN KEY (IdPessoa) REFERENCES Pessoa(Id)
 GO
-ALTER TABLE [Cidade] CHECK CONSTRAINT [FkCidade_x_Estado]
+ALTER TABLE Venda CHECK CONSTRAINT FkVenda_x_Pessoa
 
 GO
-ALTER TABLE [Endereco] WITH CHECK ADD CONSTRAINT [FkEndereco_x_Cidade] FOREIGN KEY ([IdCidade]) REFERENCES [Cidade]([Id])
+ALTER TABLE Cidade WITH CHECK ADD CONSTRAINT FkCidade_x_Estado FOREIGN KEY (IdEstado) REFERENCES Estado(Id)
 GO
-ALTER TABLE [Endereco] CHECK CONSTRAINT [FkEndereco_x_Cidade]
+ALTER TABLE Cidade CHECK CONSTRAINT FkCidade_x_Estado
 
 GO
-ALTER TABLE [Oferta] WITH CHECK ADD CONSTRAINT [FkOferta_x_Pessoa] FOREIGN KEY ([IdPessoa]) REFERENCES [Pessoa]([Id])
+ALTER TABLE Endereco WITH CHECK ADD CONSTRAINT FkEndereco_x_Cidade FOREIGN KEY (IdCidade) REFERENCES Cidade(Id)
 GO
-ALTER TABLE [Oferta] CHECK CONSTRAINT [FkOferta_x_Pessoa]
+ALTER TABLE Endereco CHECK CONSTRAINT FkEndereco_x_Cidade
 
 GO
-ALTER TABLE [ItemVenda] WITH CHECK ADD CONSTRAINT [FkItemVenda_x_Venda] FOREIGN KEY ([IdVenda]) REFERENCES [Venda]([Id])
+ALTER TABLE Oferta WITH CHECK ADD CONSTRAINT FkOferta_x_Pessoa FOREIGN KEY (IdPessoa) REFERENCES Pessoa(Id)
 GO
-ALTER TABLE [ItemVenda] CHECK CONSTRAINT [FkItemVenda_x_Venda]
-GO
-ALTER TABLE [ItemVenda] WITH CHECK ADD CONSTRAINT [FkItemVenda_x_Produto] FOREIGN KEY ([IdProduto]) REFERENCES [Produto]([Id])
-GO
-ALTER TABLE [ItemVenda] CHECK CONSTRAINT [FkItemVenda_x_Produto]
+ALTER TABLE Oferta CHECK CONSTRAINT FkOferta_x_Pessoa
 
 GO
-ALTER TABLE [Produto] WITH CHECK ADD CONSTRAINT [FkProduto_x_Categoria] FOREIGN KEY ([IdCategoria]) REFERENCES [Categoria]([Id])
+ALTER TABLE ItemVenda WITH CHECK ADD CONSTRAINT FkItemVenda_x_Venda FOREIGN KEY (IdVenda) REFERENCES Venda(Id)
 GO
-ALTER TABLE [Produto] CHECK CONSTRAINT [FkProduto_x_Categoria]
+ALTER TABLE ItemVenda CHECK CONSTRAINT FkItemVenda_x_Venda
+GO
+ALTER TABLE ItemVenda WITH CHECK ADD CONSTRAINT FkItemVenda_x_Produto FOREIGN KEY (IdProduto) REFERENCES Produto(Id)
+GO
+ALTER TABLE ItemVenda CHECK CONSTRAINT FkItemVenda_x_Produto
 
 GO
-ALTER TABLE [ContaReceber] WITH CHECK ADD CONSTRAINT [FkContaReceber_x_Venda] FOREIGN KEY ([IdVenda]) REFERENCES [Venda]([Id])
+ALTER TABLE Produto WITH CHECK ADD CONSTRAINT FkProduto_x_Categoria FOREIGN KEY (IdCategoria) REFERENCES Categoria(Id)
 GO
-ALTER TABLE [ContaReceber] CHECK CONSTRAINT [FkContaReceber_x_Venda]
-GO
-ALTER TABLE [ContaReceber] WITH CHECK ADD CONSTRAINT [FkContaReceber_x_FormaPagamento] FOREIGN KEY ([IdFormaPagamento]) REFERENCES [FormaPagamento]([Id])
-GO
-ALTER TABLE [ContaReceber] CHECK CONSTRAINT [FkContaReceber_x_FormaPagamento]
+ALTER TABLE Produto CHECK CONSTRAINT FkProduto_x_Categoria
 
 GO
-ALTER TABLE [Evento] WITH CHECK ADD CONSTRAINT [FkEvento_x_Publico] FOREIGN KEY ([IdPublico]) REFERENCES [Publico]([Id])
+ALTER TABLE ContaReceber WITH CHECK ADD CONSTRAINT FkContaReceber_x_Venda FOREIGN KEY (IdVenda) REFERENCES Venda(Id)
 GO
-ALTER TABLE [Evento] CHECK CONSTRAINT [FkEvento_x_Publico]
+ALTER TABLE ContaReceber CHECK CONSTRAINT FkContaReceber_x_Venda
 GO
-ALTER TABLE [Evento] WITH CHECK ADD CONSTRAINT [FkEvento_x_Endereco] FOREIGN KEY ([IdEnderecoPessoa]) REFERENCES [Endereco]([Id])
+ALTER TABLE ContaReceber WITH CHECK ADD CONSTRAINT FkContaReceber_x_FormaPagamento FOREIGN KEY (IdFormaPagamento) REFERENCES FormaPagamento(Id)
 GO
-ALTER TABLE [Evento] CHECK CONSTRAINT [FkEvento_x_Endereco]
+ALTER TABLE ContaReceber CHECK CONSTRAINT FkContaReceber_x_FormaPagamento
 
 GO
-ALTER TABLE [Telefone] WITH CHECK ADD CONSTRAINT [FkTelefone_x_Estado] FOREIGN KEY ([IdEstado]) REFERENCES [Estado]([Id])
+ALTER TABLE Evento WITH CHECK ADD CONSTRAINT FkEvento_x_Publico FOREIGN KEY (IdPublico) REFERENCES Publico(Id)
 GO
-ALTER TABLE [Telefone] CHECK CONSTRAINT [FkTelefone_x_Estado]
+ALTER TABLE Evento CHECK CONSTRAINT FkEvento_x_Publico
+GO
+ALTER TABLE Evento WITH CHECK ADD CONSTRAINT FkEvento_x_Endereco FOREIGN KEY (IdEnderecoPessoa) REFERENCES Endereco(Id)
+GO
+ALTER TABLE Evento CHECK CONSTRAINT FkEvento_x_Endereco
 
 GO
-ALTER TABLE [ContaPagar] WITH CHECK ADD CONSTRAINT [FkContaPagar_x_TipoContaPagar] FOREIGN KEY ([IdTipoConta]) REFERENCES [TipoContaPagar]([Id])
+ALTER TABLE Telefone WITH CHECK ADD CONSTRAINT FkTelefone_x_Estado FOREIGN KEY (IdEstado) REFERENCES Estado(Id)
 GO
-ALTER TABLE [ContaPagar] CHECK CONSTRAINT [FkContaPagar_x_TipoContaPagar]
+ALTER TABLE Telefone CHECK CONSTRAINT FkTelefone_x_Estado
+
+GO
+ALTER TABLE ContaPagar WITH CHECK ADD CONSTRAINT FkContaPagar_x_TipoContaPagar FOREIGN KEY (IdTipoConta) REFERENCES TipoContaPagar(Id)
+GO
+ALTER TABLE ContaPagar CHECK CONSTRAINT FkContaPagar_x_TipoContaPagar
