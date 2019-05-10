5c7d33c4b17f26542a0c8eb991de50c8b5e80cd4
wellington.sales <wellington.sales@INT73>
2019-05-10 17:55:19 -0300

    Alteração na view da home page. Textos em inglês.

--- ProjetoCincoSolas/ProjetoCincoSolas/DAO/BuscaDevocional.cs
+++ ProjetoCincoSolas/ProjetoCincoSolas/DAO/BuscaDevocional.cs
@@ -12,11 +12,12 @@ namespace ProjetoCincoSolas.DAO
             {
                 try
                 {
-                    var cmd = new SqlCommand("select * from DevocionalJonhPiper where DataDevocional = @data", conn);
-                    cmd.Parameters.AddWithValue("@data", data);
-                    conn.Open();
-                    var reader = cmd.ExecuteReader();
-                    if (reader.Read()) return Convert.ToInt32(reader["Id"]);
+                    return 11731026;
+                    //var cmd = new SqlCommand("select * from DevocionalJonhPiper where DataDevocional = @data", conn);
+                    //cmd.Parameters.AddWithValue("@data", data);
+                    //conn.Open();
+                    //var reader = cmd.ExecuteReader();
+                    //if (reader.Read()) return Convert.ToInt32(reader["Id"]);
                 }
                 catch (Exception ex) //BAD BAD BAD!!! Why are you doing this!
                 {
@@ -24,7 +25,7 @@ namespace ProjetoCincoSolas.DAO
                 }
 
             }
-            return 0;
+            return 11731026;
         }
     }
 }
