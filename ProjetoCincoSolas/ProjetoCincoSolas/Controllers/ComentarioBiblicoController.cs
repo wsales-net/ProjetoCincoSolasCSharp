8aeffcd53b07bbb25d59cc44924a76f6dfe8afa1
wellington.sales <wellington.sales@INT73>
2019-09-03 16:40:03 -0300

    Combobox dinamico

---
+++ ProjetoCincoSolas/ProjetoCincoSolas/Controllers/ComentarioBiblicoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoCincoSolas.Controllers
+{
+    public class ComentarioBiblicoController : Controller
+    {
+        // GET: ComentarioBiblico
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ComentarioBiblico/ListarComentarios/5
+        public ActionResult ListarComentariosCapitulo(int idCapitulo)
+        {
+            var campos = MontarComboCampo(idCapitulo);
+
+            return new JsonResult
+            {
+                Data = new
+                {
+                    campos = campos,
+                    idCampo = campos.First(),
+                    Erro = false,
+                    Mensagem = "",
+                }
+            };
+        }
+
+
+        public IList<SelectListItem> MontarComboCampo(int idTabelaCadastro)
+        {
+            //IList<Classe>
+            //var campos = new IList<obj>(); _campoTabelaCadastroRepository.BuscarCamposPorTabela(idTabelaCadastro);
+
+            //return campos.Select(x => new SelectListItem
+            //{
+            //    Value = x.Id.ToString(),
+            //    Text = x.Descricao
+            //}).ToList();
+            return new List<SelectListItem>();
+        }
+    }
+}
